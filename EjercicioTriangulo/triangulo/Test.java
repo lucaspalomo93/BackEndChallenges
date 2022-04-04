3da3da068dc043a489d6e8cdcd076e81f5000553
lucaspalomo93 <92234503+lucaspalomo93@users.noreply.github.com>
2022-04-04 15:07:04 -0300

    Add files via upload

---
+++ EjercicioTriangulo/triangulo/Test.java
@@ -0,0 +1,8 @@
+package triangulo;
+
+public class Test {
+    public static void main(String[] args) {
+        Triangulo triangulo1 = new Triangulo(2, 8, 10);
+        System.out.println(triangulo1.tipoDeTriangulo());
+    }
+}
