cc60a0a01b7ad01d14867e8b5d7fedcad3910d03
lucaspalomo93 <92234503+lucaspalomo93@users.noreply.github.com>
2022-04-04 15:08:40 -0300

    Update Test.java
    Mejora

--- EjercicioTriangulo/triangulo/Test.java
+++ EjercicioTriangulo/triangulo/Test.java
@@ -3,6 +3,11 @@ package triangulo;
 public class Test {
     public static void main(String[] args) {
         Triangulo triangulo1 = new Triangulo(2, 8, 10);
+        Triangulo triangulo2 = new Triangulo(4,4,4);
+        Triangulo triangulo3 = new Triangulo(5,5,10);
+        
         System.out.println(triangulo1.tipoDeTriangulo());
+        System.out.println(triangulo2.tipoDeTriangulo());
+        System.out.println(triangulo3.tipoDeTriangulo());
     }
 }
