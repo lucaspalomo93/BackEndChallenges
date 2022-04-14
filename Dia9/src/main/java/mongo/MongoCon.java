ab808c63a01d02c9366629bae2742dc4f0773859
lucaspalomo93 <lucaspalomo.1993@gmail.com>
2022-04-14 17:58:08 -0300

    pair

---
+++ Dia9/src/main/java/mongo/MongoCon.java
@@ -0,0 +1,11 @@
+package mongo;
+
+import com.mongodb.MongoClient;
+
+public class MongoCon {
+    public static void main(String[] args) {
+        MongoClient mongoClient = null;
+        
+        
+    }
+}
